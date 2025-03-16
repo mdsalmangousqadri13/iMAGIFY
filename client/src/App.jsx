85305b9876f123c66476140c796ee325150b0c94
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-03-17 00:31:09 +0530

    added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Result from './pages/Result'
@@ -6,12 +6,16 @@ import BuyCredit from './pages/BuyCredit'
 import Navbar from './componenet/Navbar'
 import Footer from './componenet/Footer'
 import Login from './componenet/Login'
+import { AppContext } from './context/AppContext'
 
 const App = () => {
+
+  const {showLogin} = useContext(AppContext)
+
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
       <Navbar />
-      <Login />
+      {showLogin && <Login />}
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/Result' element={<Result />}/>
@@ -21,6 +25,8 @@ const App = () => {
 
     </div>
   )
+
+
 }
 
 export default App
--- client/src/componenet/Login.jsx
+++ client/src/componenet/Login.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext';
 
 const Login = () => {
 
   const [state, setState] = useState('Login');
+  const { setShowLogin } = useContext(AppContext);
 
   useEffect(()=>{
     document.body.style.overflow = 'hidden';
@@ -43,7 +45,7 @@ const Login = () => {
         :
         <p className='mt-5 text-center'>Already have an account? <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Login')} >Login</span></p>}
 
-        <img src={assets.cross_icon} alt="" className='absolute top-5 right-5 cursor-pointer' />
+        <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" className='absolute top-5 right-5 cursor-pointer' />
       </form>
 
     </div>
