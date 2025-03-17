b7e187c11b9ed9d8b25266d1c15c6f89b7f70d60
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-03-17 14:40:41 +0530

    added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,7 +38,7 @@ const loginUser = async (req, res)=>{
         const user = await userModel.findOne({email})
 
         if(!user){
-            return res({success:false, message:'User does not exist'})
+            return res.json({success:false, message:'User does not exist'})
         }
 
         const isMatch = await bcrypt.compare(password, user.password)
@@ -48,11 +48,25 @@ const loginUser = async (req, res)=>{
 
         res.json({success: true, token, user: {name: user.name}})
         }else{
-            return res({sucess:false, message: 'Invalid credentials'})
+            return res.json({sucess:false, message: 'Invalid credentials'})
         }
     } catch (error) {
         console.log(error);
         res.json({success: false, message: error.message})
     }
 }
+      
+const userCredits = async (req, res)=>{
+    try {
+        const {userId} = req.body
+
+        const user = await userModel.findById(userId)
+        res.json({success: true, credits: user.creditBalance, user:{name: user.name}})
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { registerUser, loginUser }
         
