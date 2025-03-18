38370a6eb0221a57ebd735bb0c8216e315c010dd
md salman gous qadri <145575717+mdsalmangousqadri13@users.noreply.github.com>
2025-03-18 10:08:12 +0530

    added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useState } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 export const AppContext = createContext()
 
@@ -7,10 +9,25 @@ const AppContextProvider = (props) => {
     const [showLogin, setShowLogin] = useState(false);
     const [ token, setToken ] = useState(localStorage.getItem('token'))
 
+    
+    
     const [ credit, setCredit ] = useState(false)
-
-
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const loadCreditsData = async ()=>{
+        try {
+            const {data} = await axios.get(backendUrl +'/api/user/credits', {headers: {token}})
+
+            if(data.success){
+                setCredit(data.credits)
+                setUser(data.user)
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+            
+        }
+    }
 
     const value = {
         user, setUser, showLogin, setShowLogin, backendUrl, token, setToken, credit, setCredit
